12b78f7acc6c2ade535f1cad7c17902a6eeb782f
SsToRR <miras.ibraev.10@mail.ru>
2026-01-20 10:34:36 +0500

    Uploading lab1

---
+++ week1/Task2.java
@@ -0,0 +1,19 @@
+package week1;
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("What's the length of one side?");
+		int a = scanner.nextInt();
+		double b = a;
+		
+		System.out.println("Area: " + a*a);
+		System.out.println("Perimeter: " + 4*a);
+		System.out.println("Diagonal: " + Math.sqrt(2)*b);
+		
+	}
+
+}
