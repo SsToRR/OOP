7778f204b930560ea53ed2d55395fce61a1b85ef
SsToRR <miras.ibraev.10@mail.ru>
2026-01-26 22:09:34 +0500

    Uploading 2nd lab

---
+++ Week2/Student.java
@@ -0,0 +1,33 @@
+package Week2;
+
+public class Student {
+	
+	private String studentName;
+	private int studentID;
+	private int studentYOS;
+	
+	public Student(String name, int id, int yos) {
+		this.studentName = name;
+		this.studentID = id;
+		this.studentYOS = yos;
+	}
+	
+	public void displayDetails() {
+		System.out.println("Name - " + studentName);
+		System.out.println("ID - " + studentID);
+		System.out.println("Year of Study - " + studentYOS);
+	}
+	public void incrementID() {
+		studentID+=1;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student stud = new Student("Miras", 1, 2024);
+		
+		stud.displayDetails();
+		stud.incrementID();
+		stud.displayDetails();		
+	}
+
+}
